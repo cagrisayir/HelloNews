28112f026f0dc9fbddd1267fcf4e84c0e1eb2305
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-25 20:49:48 +0300

    Initial Commit

---
+++ HelloNews/HelloNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloNewsApp.swift
+//  HelloNews
+//
+//  Created by Omer Cagri Sayir on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
