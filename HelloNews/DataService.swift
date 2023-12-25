9ac6ea64ab83cf3dc037a8a57781c802fdb45dee
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-25 21:27:34 +0300

    app layout

---
+++ HelloNews/DataService.swift
@@ -0,0 +1,11 @@
+//
+//  DataService.swift
+//  HelloNews
+//
+//  Created by Omer Cagri Sayir on 25.12.2023.
+//
+
+import Foundation
+
+struct DataService {
+}
---
+++ HelloNews/View/SingleNewsView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleNewsView.swift
+//  HelloNews
+//
+//  Created by Omer Cagri Sayir on 25.12.2023.
+//
+
+import SwiftUI
+
+struct SingleNewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleNewsView()
+}
